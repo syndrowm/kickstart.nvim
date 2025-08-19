c4d06c38e3d862eaeec2bad5c77e4d4d469b6bc5
syndrowm <syndrowm@gmail.com>
2025-08-19 10:40:04 -0600

    add go

--- lua/custom/plugins/syn.lua
+++ lua/custom/plugins/syn.lua
@@ -1,5 +1,7 @@
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 
+local lspconfig = require 'lspconfig'
+
 local config = {
   settings = {
     ['rust-analyzer'] = {
@@ -16,7 +18,34 @@ local config = {
   },
   capabilities = capabilities,
 }
-require('lspconfig').rust_analyzer.setup(config)
+lspconfig.rust_analyzer.setup(config)
+
+local go_config = {
+  filetypes = { 'go', 'gomod' },
+  root_dir = lspconfig.util.root_pattern('go.work', 'go.mod', '.git'),
+  settings = {
+    ['gopls'] = {
+      gofumpt = true,
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      completeUnimported = true,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+  capabilities = capabilities,
+}
+
+lspconfig.gopls.setup(go_config)
 
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Search files' })
@@ -36,10 +65,29 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.shiftwidth = 4
     vim.opt_local.softtabstop = 4
     vim.opt_local.expandtab = false
-    vim.opt_local.list = true
+    vim.opt_local.list = false
   end,
 })
 
 vim.o.winborder = 'single'
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, 1000)
+    for _, res in pairs(result or {}) do
+      for _, action in pairs(res.result or {}) do
+        if action.edit then
+          vim.lsp.util.apply_workspace_edit(action.edit, 'utf-16')
+        elseif action.command then
+          vim.lsp.buf.execute_command(action.command)
+        end
+      end
+    end
+  end,
+})
 
 return {}
